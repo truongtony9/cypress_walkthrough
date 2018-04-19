4652d304a58a35a5143ca10fa86c6664951b8a5e
SheaClose <SheaClose@gmail.com>
2018-04-19 14:58:21 -0500

    don

--- src/App.js
+++ src/App.js
@@ -8,19 +8,26 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentTodo: "",
       todos: [
         {
-          title: "Teach Cypress Testing Suite",
-          currentTodo: ""
+          title: "Teach Cypress Testing Suite"
         }
       ]
     };
   }
+
+  handleChange = (prop, val) => {
+    this.setState({ [prop]: val });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header" />
         <input
+          type="text"
+          onChange={e => this.handleChange("currentTodo", e.target.value)}
           value={this.state.currentTodo}
           className="new_todo"
           autoFocus
